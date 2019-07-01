b99018b208e8214e6ba32f687c109153579ed594
Nishadi <jayasingheane@gmail.com>
2019-07-01 18:03:17 +0530

    edited

--- src/main/java/page_Elements/LoginPage_Elements.java
+++ src/main/java/page_Elements/LoginPage_Elements.java
@@ -10,19 +10,19 @@ public class LoginPage_Elements extends BaseTerms{
 	
 	public static WebElement UserName() {
 		
-		WebElement UserName = getDriver().findElement(By.xpath("/html/body/form/table/tbody/tr[1]/td[2]/input"));
+		WebElement UserName = getDriver().findElement(By.xpath("//input[@name ='uid']"));
 		return UserName;
 	}
 	
 	public static WebElement Password() {
 		
-		WebElement Password = getDriver().findElement(By.xpath("/html/body/form/table/tbody/tr[2]/td[2]/input"));
+		WebElement Password = getDriver().findElement(By.xpath("//input[@name ='password']"));
 		return Password;
 	}
 	
 	public static WebElement login() {
 		
-		WebElement login = getDriver().findElement(By.xpath("/html/body/form/table/tbody/tr[3]/td[2]/input[1]"));
+		WebElement login = getDriver().findElement(By.xpath("//input[@name ='btnLogin']"));
 		return login;
 	}
 }
